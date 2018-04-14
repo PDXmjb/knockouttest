1de33d6b035ab7c2073ebd9dc3e1a3b3698885ce
Mike <michaeljonathanbrooks+1@gmail.com>
2018-04-13 23:15:58 -0700

    Basic knockout tests

---
+++ knockouttest/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using knockouttest.Models;
+
+namespace knockouttest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public ActionResult Names()
+        {
+            return Json(new { success = true, firstName = "Hello", lastName = "World" });
+        }
+
+
+
+        public JsonResult Save(string firstName, string lastName)
+        {
+            return Json(new { success = true, fullName = firstName + " " + lastName + " SAVED" });
+        }
+    }
+
+
+}
+public class Person 
+{
+    public string firstName { get; set; }
+}
---
+++ knockouttest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace knockouttest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
